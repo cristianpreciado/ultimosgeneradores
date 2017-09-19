cb6ff854fff374880f20f79ef223ec67b2e78da5
cristianpreciadosanchez <cristianpreciadosanchez@192.168.0.5>
2017-09-18 22:36:46 -0500

    ultimos 3 generadores

---
+++ src/ultimosgeneradores/UltimosGeneradores.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ultimosgeneradores;
+
+/**
+ *
+ * @author cristianpreciadosanchez
+ */
+public class UltimosGeneradores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        principal f = new principal();
+        f.setVisible(true);
+    }
+    
+}
